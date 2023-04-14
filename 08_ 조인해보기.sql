4a9516709eed63112a0495e223141e5227706780
Kim nayeong <ny567@naver.com>
2023-04-14 17:12:07 +0900

    230414 sql

--- 08_ 조인해보기.sql
+++ 08_ 조인해보기.sql
@@ -1,91 +1,106 @@
 
--- 이너(내부) 조인
-SELECT
-    *
-FROM info i
-INNER JOIN auth a
-ON i.auth_id = a.auth_id;
+/*
+# 조인이란?
+- 서로 다른 테이블간에 설정된 관계가 결합하여
+1개 이상의 테이블에서 데이터를 조회하기 위해 사용합니다.
 
--- 오라클 문법 (잘 사용 안합니다.)
-SELECT
-    *
-FROM info i, auth a
-WHERE i.auth_id = a.auth_id;
+SELECT 컬럼리스트 FROM 조인 대상이 되는 테이블 (1개 이상)
+WHERE 조인 조건 -> 오라클 조인 
+*/
 
--- auth_id 컬럼을 그냥 쓰면 모호하다 라고 뜹니다.
--- 그 이유는 양쪽 테이블에 모두 존재하기 때문입니다.
--- 이럴 때는, 컬럼에 테이블 이름을 붙이던지, 별칭을 쓰셔서
--- 확실하게 지목을 해주세요.
+-- employees 테이블의 부서 id와 일치하는 departments 테이블의 부서 id를 찾아서
+-- SELECT 이하에 있는 컬럼들을 출력하는 쿼리문.
 SELECT
-    auth.auth_id, info.title, info.content, auth.name
-FROM info
-INNER JOIN auth 
---join auth와 같다
-ON info.auth_id = auth.auth_id;
+    first_name, last_name, hire_date,
+    salary, job_id, e.department_id, department_name
+FROM employees e, departments d
+WHERE e.department_id = d.department_id; -- 오라클 조인
 
 SELECT
-    a.auth_id, i.title, i.content, a.name
-FROM info i
-INNER JOIN auth a
-ON i.auth_id = a.auth_id;
-
--- 필요한 데이터만 조회하겠다! 라고 한다면
--- WHERE 구문을 통해 일반 조건을 걸어 주시면 됩니다.
-SELECT
-    a.auth_id, i.title, i.content, a.name
-FROM info i
-INNER JOIN auth a
-ON i.auth_id = a.auth_id
-where a.name = '이순신';
-
--- 아우터(외부) 조인
-SELECT
-    *
-FROM info i right JOIN auth a
--- right after join과 같다
-ON i.auth_id = a.auth_id;
-
-SELECT
-    *
-from info i, auth a
-where i.auth_id = a.auth_id(+);
--- 위와 같다 위- ansi 아래- oracle 문법
-
--- 좌측 테이블과 우측 테이블 데이터를 모두 읽어 중복된 데이터는 삭제되는 외부 조인.
-SELECT *
-FROM info i FULL JOIN auth a
-ON i.auth_id = a.auth_id;
-
---아우터 조인 종류 : left/ right/ full
-
--- CROSS JOIN은 JOIN조건을 설정하지 않기 때문에
--- 단순히 모든 컬럼에 대해 JOIN을 진행합니다.
--- 실제로는 거의 사용하지 않습니다.
-
-SELECT * FROM info
-CROSS JOIN auth
-ORDER BY id ASC;
-
--- 여러 개 테이블 조인 -> 키 값만 찾아서 구문을 연결해서 쓰시면 됩니다.
-SELECT
-    *
+    e.first_name, e.last_name, e.hire_date,
+    e.salary, e.job_id, d.department_id, d.department_name
 FROM employees e
-LEFT JOIN departments d ON e.department_id = d.department_id
-LEFT JOIN locations loc ON d.location_id = loc.location_id;
+INNER JOIN departments d
+ON e.department_id = d.department_id;
 
 /*
-- 테이블 별칭 a, i를 이용하여 LEFT OUTER JOIN 사용.
-- info, auth 테이블 사용
-- job 컬럼이 scientist인 사람의 id, title, content, job을 출력.
+각각의 테이블에 독립적으로 존재하는 컬럼의 경우에는
+테이블 이름을 생략해도 무방합니다. 그러나, 해석의 명확성을 위해
+테이블 이름을 작성하셔서 소속을 표현해 주는 것이 바람직합니다.
+테이블 이름이 너무 길 시에는 ALIAS를 작성하여 칭합니다.
+두 테이블 모두 가지고 있는 컬럼의 경우 반드시 명시해 주셔야 합니다.
+*/
+
+-- 3개의 테이블을 이용한 내부 조인 (INNER JOIN)
+-- 내부 조인: 두 테이블 모두에서 일치하는 값을 가진 행만 반환합니다.
+SELECT
+    e.first_name, e.last_name, e.department_id,
+    d.department_name, 
+    j.job_title
+FROM employees e, departments d, jobs j
+WHERE e.department_id = d.department_id
+AND e.job_id = j.job_id;
+
+SELECT
+    e.first_name, e.last_name,
+    d.department_id, d.department_name,
+    j.job_title,
+    loc.city
+FROM
+    employees e,
+    departments d,
+    jobs j,
+    locations loc
+WHERE
+    e.department_id = d.department_id --4
+AND
+    e.job_id = j.job_id --3
+AND
+    d.location_id = loc.location_id --2
+AND
+    loc.state_province = 'California'; -- 1
+    
+/*
+1. loc 테이블의 province = 'California' 조건에 맞는 값을 대상으로
+2. location_id 값과 같은 값을 가지는 데이터를
+    departments에서 찾아서 조인
+3. 위의 결과와 동일한 department_id를 가진
+    employees 테이블의 데이터를 찾아서 조인
+4. 위의 결과와 jobs 테이블을 비교하여 조인하고 최종 결과를 출력.
+*/
+
+
+-- 외부 조인
+/*
+상호 테이블간에 일치되는 값으로 연결되는 내부 조인과는 다르게
+어느 한 테이블에 공통 값이 없더라도 해당 row들이 조회 결과에 
+모두 포함되는 조인을 말합니다.
+*/ 
+
+SELECT
+    e.first_name, e.last_name, e.hire_date,
+    e.salary, e.job_id, e.department_id, d.department_name
+FROM employees e, departments d, locations loc
+WHERE e.department_id = d.department_id(+)
+AND d.location_id = loc.location_id;
+/*
+employees 테이블에는 존재하고, departments 테이블에는 존재하지 않아도
+(+)가 붙지 않은 테이블을 기준으로 하여 departments 테이블이 조인에
+참여하라는 의미를 부여하기 위해 기호를 붙입니다.
+외부 조인을 사용했더라도, 이후에 내부 조인을 사용하면
+내부조인을 우선적으로 인식합니다.
+*/
+
+/*
+-- 외부 조인 진행 시 모든 조건에 (+)를 붙여야 외부 조인이 유지됩니다.
+-- 일반 조건에도 (+)를 붙이지 않으면 외부 조인이 풀리는 현상이 발생.(데이터 누락)
 */
 
 SELECT
-    i.id,
-    i.title,
-    i.content,
-    a.job
-FROM auth a LEFT JOIN info i
-ON a.auth_id = i.auth_id
-WHERE a.job = 'scientist';
-
+    e.employee_id, e.first_name,
+    e.department_id,
+    j.start_date, j.end_date, j.job_id
+FROM employees e, job_history j
+WHERE e.employee_id = j.employee_id(+)
+AND j.department_id(+) = 80;
 
