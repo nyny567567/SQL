9d77ad683931f3a28ad828bc11124c06a6c03e26
ny <ny567@naver.com>
2023-04-21 18:16:18 +0900

    230421

---
+++ hr_practice.sql
@@ -0,0 +1,10 @@
+
+DROP TABLE members;
+
+CREATE TABLE members(
+    mem_id VARCHAR2(20) PRIMARY KEY,
+    mem_PW VARCHAR2(20) NOT NULL,
+    mem_name VARCHAR2(20) NOT NULL,
+    mem_age NUMBER(2),
+    mem_regdate DATE DEFAULT sysdate
+);
